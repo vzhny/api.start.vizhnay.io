800df1885e1f84dfdef31b4c58dcf793d7af5302
Diego Vizhnay <diego@vzhny.com>
2019-03-22 13:14:02 -0400

    📝 Created (empty) controller definitions | 03/22

---
+++ src/api/controllers/link.controllers.js
@@ -0,0 +1,20 @@
+/* eslint-disable */
+
+import to from 'await-to-js';
+import bcrypt from 'bcryptjs';
+import User from '@/models/user.model';
+import Link from '@/models/link.model';
+import Category from '@/models/category.model';
+import handleError from '@/api/helpers/handleError';
+
+/* eslint-disable consistent-return */
+
+export const getAllLinks = async (req, res) => {};
+
+export const getOneLink = async (req, res) => {};
+
+export const addNewLink = async (req, res) => {};
+
+export const updateLink = async (req, res) => {};
+
+export const deleteLink = async (req, res) => {};
