dc239599eea407fa52d8c758bf620c99b650522c
Diego Vizhnay <diego@vzhny.com>
2019-03-22 12:43:38 -0400

    🎉 Initial link migration commit! | 03/22

---
+++ src/db/migrations/20190322111214_link.js
@@ -0,0 +1,25 @@
+exports.up = knex => {
+  return knex.schema.createTable('links', table => {
+    table.increments('id');
+    table.string('url').notNullable();
+    table.string('name', 16).notNullable();
+    table
+      .string('owner')
+      .references('userId')
+      .inTable('users')
+      .notNullable();
+    table
+      .integer('category')
+      .references('id')
+      .inTable('categories')
+      .notNullable();
+    table
+      .timestamp('createdAt')
+      .defaultTo(knex.fn.now())
+      .notNullable();
+
+    table.unique('url');
+  });
+};
+
+exports.down = knex => knex.schema.dropTable('links');
