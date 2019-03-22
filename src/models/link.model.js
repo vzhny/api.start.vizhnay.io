8a0d7b43ced6f0a3db2f2199e8f0b9ca4697bdc8
Diego Vizhnay <diego@vzhny.com>
2019-03-22 12:45:03 -0400

    🎉 Initial link model commit! | 03/22

---
+++ src/models/link.model.js
@@ -0,0 +1,21 @@
+import { Model } from 'objection';
+
+class Category extends Model {
+  static tableName = 'users';
+
+  static jsonSchema = {
+    type: 'object',
+    required: ['name', 'url'],
+
+    properties: {
+      id: { type: 'serial' },
+      name: { type: 'string', maxLength: 16 },
+      url: { type: 'string', unique: true },
+      owner: { type: 'string' },
+      category: { type: 'integer' },
+      createdAt: { type: 'timestamptz' },
+    },
+  };
+}
+
+export default Category;
