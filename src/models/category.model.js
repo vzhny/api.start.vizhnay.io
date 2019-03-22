61df6250ef30755d22508823f9e452be0a79e5e5
Diego Vizhnay <diego@vzhny.com>
2019-03-22 12:45:16 -0400

    🎉 Initial category model commit! | 03/22

---
+++ src/models/category.model.js
@@ -0,0 +1,18 @@
+import { Model } from 'objection';
+
+class Category extends Model {
+  static tableName = 'users';
+
+  static jsonSchema = {
+    type: 'object',
+    required: 'name',
+
+    properties: {
+      id: { type: 'serial' },
+      name: { type: 'string', unique: true, maxLength: 24 },
+      createdAt: { type: 'timestamptz' },
+    },
+  };
+}
+
+export default Category;
