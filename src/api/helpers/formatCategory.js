c264a9e2796633fab099833e67c1ec0272ab4e2b
Diego Vizhnay <diego@vzhny.com>
2019-03-23 19:50:02 -0400

    🔥 Removed formatCategory helper function -- incorporated into handleCategory helper file | 03/23

--- src/api/helpers/formatCategory.js
+++
@@ -1,9 +0,0 @@
-export default category => {
-  if (typeof category !== 'string') {
-    return '';
-  }
-
-  const trimmedCategory = category.trim();
-
-  return `${trimmedCategory.charAt(0).toUpperCase()}${trimmedCategory.slice(1)}`;
-};
